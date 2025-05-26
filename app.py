579de51cbcec4ae70a7444b4394222f8e925a44f
bimbo-22 <bimbimuhad@gmail.com>
2025-05-26 19:46:56 +0200

    test app with csv prediction

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ mongo_db_url = os.getenv("MONGO_DB_URL")
 print(mongo_db_url)
 
 import pymongo
+import pandas as pd
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.pipeline.training_pipeline import TrainingPipeline
@@ -18,11 +19,12 @@ from fastapi import FastAPI, File, UploadFile, Request
 from uvicorn import run as app_run
 from fastapi.responses import Response
 from starlette.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates # for rendering HTML templates
 
 import pandas as pd
 
 from networksecurity.utils.main_utils.utils import load_object
-
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 from networksecurity.constants.training_pipeline import DATA_INGESTION_COLLECTION_NAME
 from networksecurity.constants.training_pipeline import DATA_INGESTION_DATABASE_NAME
 
@@ -58,6 +60,29 @@ async def train_route():
         return Response("Training Successful")
     except Exception as e :
         raise NetworkSecurityException (e,sys)
+    
+    
+templates = Jinja2Templates(directory="./templates")
+@app.get("/predict")
+async def predict_route(request: Request, file: UploadFile = File(...)):
+    try:
+        df = pd.read_csv(file.file)
+        
+        preprocessor = load_object("final_model/preprocessor.pkl")
+        final_model = load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocessor, final_model = final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        df["prediction_column"] = y_pred
+        print(df["prediction_column"])
+        # df["predicted_column"].replace(-1,0)
+        # return df.to_json()
+        df.to_csv("prediction_output/output.csv")
+        table_html = df.to_html(classes='table table-striped', index=False)
+        return templates.TemplateResponse("predict.html", {"request": request, "table_html": table_html})
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
 # @app.get("/batch_prediction")
 # async def
     
